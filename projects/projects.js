5a82abb7b84bb85fa5072be4c13db97c3c21ceeb
ericafk0001 <dxslyofficial@gmail.com>
2024-11-02 21:29:11 -0400

    tailwindcss

--- projects/projects.js
+++
@@ -1,5 +0,0 @@
-console.warn(
-  '%cNote',
-  'color: purple; font-weight: 600; background: yellow; padding: 0 5px; border-radius: 5px',
-  'Hello! Please do not steal my website and claim it as your own\n'
-);
