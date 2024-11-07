b490724f126f0139187c7e844eab3fd8a796c3e1
ericafk0001 <dxslyofficial@gmail.com>
2024-11-06 21:03:51 -0500

    skills btn smooth scroll etc

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+document
+  .querySelector(".arrow-container")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); //prevent link behaviour
+    const target = document.querySelector(this.getAttribute("href"));
+
+    window.scrollTo({
+      top: target.offsetTop - 150, // Adjust
+      behavior: "smooth",
+    });
+  });
+
 console.warn(
   " |\\__/,|   (`\\\r\n |_ _  |.--.) )\r\n ( T   )     /\r\n(((^_(((/(((_/\r\n"
 );
+console.log("credits to andy for the meow meow");
