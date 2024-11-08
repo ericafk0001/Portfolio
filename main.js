5433ee2e9875e7c6a749919154a27cfcdfc4739d
ericafk0001 <dxslyofficial@gmail.com>
2024-11-07 20:38:43 -0500

    projects section and small edits to text

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
+document.querySelectorAll(".arrow-container, .skillsBtn").forEach((element) => {
+  element.addEventListener("click", function (event) {
+    event.preventDefault();
+    const targetSelector = this.getAttribute("href");
+    const target = document.querySelector(targetSelector);
+
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 150, // Adjust
+        behavior: "smooth",
+      });
+    }
+  });
+});
+
 document
-  .querySelector(".arrow-container")
+  .querySelector(".projectsBtn")
   .addEventListener("click", function (event) {
     event.preventDefault(); //prevent link behaviour
     const target = document.querySelector(this.getAttribute("href"));
 
     window.scrollTo({
-      top: target.offsetTop - 150, // Adjust
+      top: target.offsetTop - 169, // Adjust
       behavior: "smooth",
     });
   });
