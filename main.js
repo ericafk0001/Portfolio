bda69355b91ddb3d98b880ad8052291ff6290f10
ericafk0001 <dxslyofficial@gmail.com>
2024-11-10 21:56:31 -0500

    got to fix the navbar tmr

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  })
+);
+
 document.querySelectorAll(".arrow-container, .skillsBtn").forEach((element) => {
   element.addEventListener("click", function (event) {
     event.preventDefault();
